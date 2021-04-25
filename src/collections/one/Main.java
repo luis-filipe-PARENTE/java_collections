4733166a693736d24d18386dd1b2fc66e51398bf
Luis-Filipe PARENTE <lfmparente@gmail.com>
2021-04-25 08:15:31 +0200

    collections overview

---
+++ src/collections/one/Main.java
@@ -0,0 +1,21 @@
+package collections.one;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Theatre theatre = new Theatre("Olympian", 8, 12);
+        theatre.getSeats();
+
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+        
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
