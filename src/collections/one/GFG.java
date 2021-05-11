b06e3cc38018422e198b977e1d50895635f3b9d4
Luis-Filipe PARENTE <lfmparente@gmail.com>
2021-05-12 00:47:31 +0200

    154. Binary Search

---
+++ src/collections/one/GFG.java
@@ -0,0 +1,138 @@
+package collections.one;
+
+//Java program to demonstrate working of Collections.
+//binarySearch()
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class GFG {
+
+	public static void main(String[] args) {
+//		sample1();
+//		sample2();
+//		sample3();
+		sample4();
+	}
+
+	private static void sample1() {
+		System.out.println("--start----------- sample 1 ---------------------------");
+
+		List<Integer> al = new ArrayList<Integer>();
+		al.add(1);
+		al.add(2);
+		al.add(3);
+		al.add(10);
+		al.add(20);
+
+		// 10 is present at index 3.
+		int index = Collections.binarySearch(al, 10);
+		System.out.println(index);
+
+		// 13 is not present. 13 would have been inserted
+		// at position 4. So the function returns (-4-1)
+		// which is -5.
+		index = Collections.binarySearch(al, 13);
+
+		System.out.println(index);
+		System.out.println("--end----------- sample 1 ---------------------------");
+	}
+
+	private static void sample2() {
+		System.out.println("--start----------- sample 2 ---------------------------");
+
+		List<Integer> al = new ArrayList<Integer>();
+
+		al.add(100);
+		al.add(30);
+		al.add(50);
+		al.add(8);
+		al.add(10);
+		al.add(2);
+
+		al.forEach(System.out::println);
+
+		int index = Collections.binarySearch(al, 50, Collections.reverseOrder());
+
+		System.out.println("Found at index " + index);
+		System.out.println("--end----------- sample 2 ---------------------------");
+	}
+
+	private static void sample3() {
+		System.out.println("--start----------- sample 3 ---------------------------");
+
+		List<Integer> al = new ArrayList<Integer>();
+
+		al.add(2);
+		al.add(8);
+		al.add(10);
+		al.add(30);
+		al.add(50);
+		al.add(100);
+
+		al.forEach(System.out::println);
+
+		int index = Collections.binarySearch(al, 50);
+
+		System.out.println("Found at index " + index);
+		System.out.println("--end----------- sample 3 ---------------------------");
+	}
+
+	private static void sample4() {
+		System.out.println("--start----------- sample 4 ---------------------------");
+
+		// Create a list
+		List<Domain> l = new ArrayList<Domain>();
+		l.add(new Domain(10, "quiz.geeksforgeeks.org"));
+		l.add(new Domain(20, "practice.geeksforgeeks.org"));
+		l.add(new Domain(30, "code.geeksforgeeks.org"));
+		l.add(new Domain(40, "www.geeksforgeeks.org"));
+		l.add(new Domain(5, "www.parenteIsGeek.org"));
+
+		Comparator<Domain> c = new Comparator<Domain>() {
+			public int compare(Domain u1, Domain u2) {
+				return u1.getId().compareTo(u2.getId());
+			}
+		};
+		
+		
+
+		// Searching a domain with key value 10. To search
+		// we create an object of domain with key 10.
+		int index = Collections.binarySearch(l, new Domain(40, null), (Domain u1, Domain u2) -> u1.getId().compareTo(u2.getId()));
+		System.out.println("Found at index  " + index);
+		
+		int indexBis = Collections.binarySearch(l, new Domain(40, null), (Domain u1, Domain u2) -> u1.getId().compareTo(u2.getId()));
+		System.out.println("Found at indexBis  " + indexBis);
+
+		// Searching an item with key 5
+		index = Collections.binarySearch(l, new Domain(5, null), c);
+		System.out.println(index);
+
+		System.out.println("--end----------- sample 4 ---------------------------");
+	}
+
+}
+
+//A user-defined class to store domains with id and url
+class Domain implements Comparable<Domain>{
+	private int id;
+	private String url;
+
+// Constructor
+	public Domain(int id, String url) {
+		this.id = id;
+		this.url = url;
+	}
+
+	public Integer getId() {
+		return Integer.valueOf(id);
+	}
+
+	@Override
+	public int compareTo(Domain another) {
+		return new Integer(this.id).compareTo(another.getId());
+			
+	}
+}
--- src/collections/one/Theatre.java
+++ src/collections/one/Theatre.java
@@ -1,108 +1,107 @@
 package collections.one;
 
-
 import java.util.*;
 
 /**
  * Created by dev on 2/12/2015.
  */
 public class Theatre {
-    private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+	private final String theatreName;
 
-    public Theatre(String theatreName, int numRows, int seatsPerRow) {
-        this.theatreName = theatreName;
-        this.init(numRows, seatsPerRow);
-    }
-    
-    private void init( int numRows, int seatsPerRow) {
-        int lastRow = 'A' + (numRows -1);
-       
-        for (char row = 'A'; row <= lastRow; row++) {
-            for(int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
-                Seat seat = new Seat(row + String.format("%02d", seatNum));
-                seats.add(seat);
-            }
-        }
-    }
+	// https://beginnersbook.com/2013/12/difference-between-arraylist-and-linkedlist-in-java/
+	private List<Seat> seats = new ArrayList<>();
 
-    public String getTheatreName() {
-        return theatreName;
-    }
+	public Theatre(String theatreName, int numRows, int seatsPerRow) {
+		this.theatreName = theatreName;
+		this.init(numRows, seatsPerRow);
+	}
 
-    public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for(Seat seat : seats) {
-            if(seat.getSeatNumber().equals(seatNumber)) {
-                requestedSeat = seat;
-                break;
-            }
-        }
+	private void init(int numRows, int seatsPerRow) {
+		int lastRow = 'A' + (numRows - 1);
 
-        if(requestedSeat == null) {
-            System.out.println("There is no seat " + seatNumber);
-            return false;
-        }
+		for (char row = 'A'; row <= lastRow; row++) {
+			for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+				Seat seat = new Seat(row + String.format("%02d", seatNum));
+				seats.add(seat);
+			}
+		}
+	}
 
-        return requestedSeat.reserve();
-    }
+	public String getTheatreName() {
+		return theatreName;
+	}
 
-    // for testing
-    public void getSeats() {
-        for(Seat seat : seats) {
-            System.out.println(seat.getSeatNumber());
-        }
-    }
+	public boolean reserveSeat(String seatNumber) {
+		Seat requestedSeat = new Seat(seatNumber);
+		int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+		
+		if(foundSeat >= 0) {
+			return seats.get(foundSeat).reserve();
+		} else {
+			System.out.println("There is no seat " + seatNumber);
+			return false;
+		}
 
-    private class Seat {
-        private final String seatNumber;
-        private boolean reserved = false;
+//		for (Seat seat : seats) {
+//			System.out.print(".");
+//			if (seat.getSeatNumber().equals(seatNumber)) {
+//				requestedSeat = seat;
+//				break;
+//			}
+//		}
 
-        public Seat(String seatNumber) {
-            this.seatNumber = seatNumber;
-        }
+//		if (requestedSeat == null) {
+//			System.out.println("There is no seat " + seatNumber);
+//			return false;
+//		}
 
-        public boolean reserve() {
-            if(!this.reserved) {
-                this.reserved = true;
-                System.out.println("Seat " + seatNumber + " reserved");
-                return true;
-            } else {
-                return false;
-            }
-        }
+//		return requestedSeat.reserve();
+	}
 
-        public boolean cancel() {
-            if(this.reserved) {
-                this.reserved = false;
-                System.out.println("Reservation of seat " + seatNumber + " cancelled");
-                return true;
-            } else {
-                return false;
-            }
-        }
+	// for testing
+	public void getSeats() {
+		for (Seat seat : seats) {
+			System.out.println(seat.getSeatNumber());
+		}
+	}
 
-        public String getSeatNumber() {
-            return seatNumber;
-        }
-    }
+	private class Seat implements Comparable<Seat> {
+		private final String seatNumber;
+		private boolean reserved = false;
 
+		public Seat(String seatNumber) {
+			this.seatNumber = seatNumber;
+		}
 
+		public boolean reserve() {
+			if (!this.reserved) {
+				this.reserved = true;
+				System.out.println("Seat " + seatNumber + " reserved");
+				return true;
+			} else {
+				return false;
+			}
+		}
 
+		public boolean cancel() {
+			if (this.reserved) {
+				this.reserved = false;
+				System.out.println("Reservation of seat " + seatNumber + " cancelled");
+				return true;
+			} else {
+				return false;
+			}
+		}
 
+		public String getSeatNumber() {
+			return seatNumber;
+		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		@Override
+		public int compareTo(Seat seat) {
+			int result = this.getSeatNumber().compareToIgnoreCase(seat.getSeatNumber());
+			System.out.println("compareTo ...");
+			return result;
+		}
+	}
 }
